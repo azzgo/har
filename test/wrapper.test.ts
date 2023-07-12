f63dbd28ff589ccd648749cbf5a410f2a1231740
Ison <azzgo@users.noreply.github.com>
2023-07-12 21:47:20 +0800

    refactor: wrap HarWrapper test

--- test/wrapper.test.ts
+++ test/wrapper.test.ts
@@ -1,6 +1,11 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { EntryWrapper, HarWrapper, PageWrapper } from "../src/wrapper";
-import { createEntry, createHar, createPage, createRequest } from "./test-factories";
+import {
+  createEntry,
+  createHar,
+  createPage,
+  createRequest,
+} from "./test-factories";
 
 test("create a Har Wrapper", () => {
   const har = {
@@ -16,57 +21,59 @@ test("create a Har Wrapper", () => {
   expect(wrapper.creator.name).toEqual("Firebug");
 });
 
-test("can get Entry Wrapper", () => {
-  const wrapper = new HarWrapper(
-    createHar({
-      entries: [createEntry({})],
-    })
-  );
+describe("Har Wrapper", () => {
+  test("can get Entry Wrapper", () => {
+    const wrapper = new HarWrapper(
+      createHar({
+        entries: [createEntry({})],
+      })
+    );
 
-  expect(wrapper.entryWrapper).toBeInstanceOf(EntryWrapper);
+    expect(wrapper.entryWrapper).toBeInstanceOf(EntryWrapper);
+  });
+
+  test("can get Entry Wrapper", () => {
+    const wrapper = new HarWrapper(
+      createHar({
+        pages: [createPage({})],
+      })
+    );
+
+    expect(wrapper.entryWrapper).toBeInstanceOf(EntryWrapper);
+  });
+
+  test("can get Page Wrapper", () => {
+    const wrapper = new HarWrapper(
+      createHar({
+        entries: [createEntry({})],
+      })
+    );
+
+    expect(wrapper.pageWrapper).toBeInstanceOf(PageWrapper);
+  });
 });
 
-test("can get Entry Wrapper", () => {
-  const wrapper = new HarWrapper(
-    createHar({
-      pages: [createPage({})],
-    })
-  );
+test("EntryWrapper can filter entry by HTTP method", () => {
+  const entry1 = createEntry({ request: createRequest({ method: "POST" }) });
+  const entry2 = createEntry({ request: createRequest({ method: "POST" }) });
+  const entry3 = createEntry({ request: createRequest({ method: "GET" }) });
+  const entry4 = createEntry({ request: createRequest({ method: "PATCH" }) });
+  const wrapper = new EntryWrapper([entry1, entry2, entry3, entry4]);
 
-  expect(wrapper.entryWrapper).toBeInstanceOf(EntryWrapper);
+  const postEntries = wrapper.filterByHttpMethod("POST");
+  expect(postEntries.length).toEqual(2);
+  expect(postEntries.indexOf(entry1)).greaterThan(-1);
+  expect(postEntries.indexOf(entry2)).greaterThan(-1);
+
+  const getEntries = wrapper.filterByHttpMethod("GET");
+  expect(getEntries.length).toEqual(1);
+  expect(getEntries[0]).toEqual(entry3);
+
+  const patchEntries = wrapper.filterByHttpMethod("patch");
+  expect(patchEntries.length).toEqual(1);
+  expect(patchEntries[0]).toEqual(entry4);
+
+  expect(wrapper.filterByHttpMethod("delete")).toEqual([]);
+  expect(wrapper.filterByHttpMethod("HEAD")).toEqual([]);
+  expect(wrapper.filterByHttpMethod("put")).toEqual([]);
 });
-
-test("can get Page Wrapper", () => {
-  const wrapper = new HarWrapper(
-    createHar({
-      entries: [createEntry({})],
-    })
-  );
-
-  expect(wrapper.pageWrapper).toBeInstanceOf(PageWrapper);
-});
-
-test('EntryWrapper can filter entry by HTTP method', () => {
-  const entry1 = createEntry({ request: createRequest({ method: 'POST' }) })
-  const entry2 = createEntry({ request: createRequest({ method: 'POST' }) })
-  const entry3 = createEntry({ request: createRequest({ method: 'GET' }) })
-  const entry4 = createEntry({ request: createRequest({ method: 'PATCH' }) })
-  const wrapper = new EntryWrapper([entry1, entry2, entry3, entry4])
-
-  const postEntries = wrapper.filterByHttpMethod('POST')
-  expect(postEntries.length).toEqual(2)
-  expect(postEntries.indexOf(entry1)).greaterThan(-1)
-  expect(postEntries.indexOf(entry2)).greaterThan(-1)
-  
-  const getEntries = wrapper.filterByHttpMethod('GET')
-  expect(getEntries.length).toEqual(1)
-  expect(getEntries[0]).toEqual(entry3)
-
-  const patchEntries = wrapper.filterByHttpMethod('patch')
-  expect(patchEntries.length).toEqual(1)
-  expect(patchEntries[0]).toEqual(entry4)
-
-  expect(wrapper.filterByHttpMethod('delete')).toEqual([])
-  expect(wrapper.filterByHttpMethod('HEAD')).toEqual([])
-  expect(wrapper.filterByHttpMethod('put')).toEqual([])
-})
