707b93512028c71c002e4a74959f497f4ff4cb19
Ison <azzgo@users.noreply.github.com>
2023-07-13 20:49:03 +0800

    chore: add homepage and export Exceptions

--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,4 @@
 export { fromPath, fromString } from "./reader";
+export { InvalidHarFormat, UnsupportSpecVersion } from 'error';
 export { HarWrapper, PageWrapper, EntryWrapper } from './wrapper';
 export * from "./type";
