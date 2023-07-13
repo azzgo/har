efd1c71fbfc2cbb8dafe991d262511ee46084ffb
Ison <azzgo@users.noreply.github.com>
2023-07-13 20:45:00 +0800

    chore: use ts parse schema and add coverage in test script

--- src/validator.ts
+++ src/validator.ts
@@ -1,23 +1,21 @@
-import path from 'path'
-import Ajv, { ValidateFunction } from 'ajv'
-import fs from 'fs'
-import {Har} from './type'
+import Ajv, { ValidateFunction } from "ajv";
+import { Har } from "./type";
+import HarSchema from "./har-schema.json";
 
 class Validator {
-  private validate: ValidateFunction
- 
+  private validate: ValidateFunction;
+
   constructor() {
-    const Schema = fs.readFileSync(path.resolve(__dirname, './har-schema.json'), { encoding: 'utf-8' })
-    this.validate = new Ajv().compile(JSON.parse(Schema))
+    this.validate = new Ajv().compile(HarSchema);
   }
 
   verify(json: any): boolean {
-    return this.validate(json)
+    return this.validate(json);
   }
 
   checkVersion(har: Har): boolean {
-    return har.log.version.trim() === "1.2"
+    return har.log.version.trim() === "1.2";
   }
 }
 
-export default new Validator()
+export default new Validator();
